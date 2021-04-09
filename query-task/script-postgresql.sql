35ca01c5846ab4d888a1d1d2d89dcd8a908c4c27
hariszaki17 <hariszaki17@gmail.com>
2021-04-09 22:37:06 +0700

    query-task-insert-data

--- query-task/script-postgresql.sql
+++ query-task/script-postgresql.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS users (
         "UserName" VARCHAR(50) NOT NULL,
         "Parent" INTEGER
 );
+
+INSERT INTO users ("ID", "UserName", "Parent")
+VALUES(1, 'Ali', null);
+INSERT INTO users ("ID", "UserName", "Parent")
+VALUES(2, 'Budi', null);
+INSERT INTO users ("ID", "UserName", "Parent")
+VALUES(3, 'Cecep', null);
